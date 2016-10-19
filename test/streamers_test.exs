6b698969735d494e30a64baeb3e488aea9135106
Luciano Sousa <ls@lucianosousa.net>
2016-10-18 23:32:32 -0200

    add test case for not found index file

--- test/streamers_test.exs
+++ test/streamers_test.exs
@@ -6,4 +6,8 @@ defmodule StreamersTest do
     assert Streamers.find_index('test/fixtures/emberjs') ==
       "9af0270acb795f9dcafb5c51b1907628.m3u8"
   end
+
+  test "returns nil for not available index file" do
+    assert Streamers.find_index('test/fixtures/not_available') == nil
+  end
 end
