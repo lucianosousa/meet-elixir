a3daaac969cb21f823298ec9353e5b48c5b9fdd2
Luciano Sousa <ls@lucianosousa.net>
2016-10-18 23:28:31 -0200

    looking for a file in a directory

--- test/streamers_test.exs
+++ test/streamers_test.exs
@@ -2,7 +2,8 @@ defmodule StreamersTest do
   use ExUnit.Case
   doctest Streamers
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "find index file in a directory" do
+    assert Streamers.find_index('test/fixtures/emberjs') ==
+      "9af0270acb795f9dcafb5c51b1907628.m3u8"
   end
 end
